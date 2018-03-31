fe584490d055aa8ea629e6e8495c38567220b2e4
Art <art@stoupin.ru>
2018-03-31 15:24:54 +0300

    Init

---
+++ MyMapStruct/src/main/java/MyMapInterface.java
@@ -0,0 +1,15 @@
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MyMapInterface {
+	MyMapInterface INSTANCE = Mappers.getMapper( MyMapInterface.class );
+	
+	@Mappings( {
+		@Mapping(target = "field2", expression = "java(src.field2+\"!\")"),
+		@Mapping(target = "field3", source="field3", dateFormat="dd.MM.yyyy")
+	})
+    MyDestClass sourceToDest(MySourceClass src); 
+}
